7877c1ca82bbf0bd967c58414661caac94dff4f5
Asep Saepuloh <asep.cgeds@gmail.com>
2024-06-13 14:00:29 +0700

    feat: add content dummy reg and submission

--- resources/views/livewire/pages/registration.blade.php
+++ resources/views/livewire/pages/registration.blade.php
@@ -1,12 +1,192 @@
 <div>
-    <div class="hero min-h-screen bg-base-200">
-        <div class="hero-content flex-col lg:flex-row">
-            <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
-            <div>
-                <h1 class="text-5xl font-bold">Registration!</h1>
-                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                <a href="#welcome-message" class="btn btn-primary">Get Started</a>
+    <div class="pt-16 pb-52 lg:px-20 px-5">
+        <div class="flex-col flex gap-3 mb-10">
+            <h4 class="text-2xl text-primary-600 font-semibold">Registration</h4>
+            <h1 class="text-4xl text-primary-700 font-semibold">Registration Fee</h1>
+            <a href="#reg-info">reg info</a>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-7 lg:gap-10">
+            <div class="card bg-price shadow-xl">
+                {{-- <figure class="relative"><img src="images/pricing_bg.jpg" alt="" />
+                </figure> --}}
+                <div class="card-body w-full">
+                    <h2 class="card-title text-primary-500 text-4xl">Specialist</h2>
+                    <div class="border my-5 border-primary-600"></div>
+                    <div class="grid grid-cols-2 gap-5  w-full mx-3 text-slate-600 font-semibold">
+                        <p>Symposium</p>
+                        <p>Rp. 1.500.000</p>
+                        <p>Symposium +1
+                            Workshop non
+                            Hands- On</p>
+                        <p>Rp. 2.500.000</p>
+    
+                        <p>Symposium
+                            +1
+                            Workshop
+                            Hands-On</p>
+                        <p>Rp. 3.500.000</p>
+    
+                        <p>Extra
+                          Workshop
+                          Non Hands-
+                          On</p>
+                        <p>Rp. 1.000.000</p>
+                        <p>Extra
+                            Worksho
+                            pHands-
+                            On</p>
+                        <p>Rp. 2.000.000</p>
+                    </div>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-price shadow-xl">
+                {{-- <figure class="relative"><img src="images/pricing_bg.jpg" alt="" />
+                </figure> --}}
+                <div class="card-body w-full">
+                    <h2 class="card-title text-primary-500 text-4xl">Specialist</h2>
+                    <div class="border my-5 border-primary-600"></div>
+                    
+                    <div class="grid grid-cols-2 gap-5  w-full mx-3 text-slate-600 font-semibold">
+                        <p>Symposium</p>
+                        <p>Rp. 1.500.000</p>
+                        <p>Symposium +1
+                            Workshop non
+                            Hands- On</p>
+                        <p>Rp. 2.500.000</p>
+    
+                        <p>Symposium
+                            +1
+                            Workshop
+                            Hands-On</p>
+                        <p>Rp. 3.500.000</p>
+    
+                        <p>Extra
+                          Workshop
+                          Non Hands-
+                          On</p>
+                        <p>Rp. 1.000.000</p>
+                        <p>Extra
+                            Worksho
+                            pHands-
+                            On</p>
+                        <p>Rp. 2.000.000</p>
+                    </div>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-price shadow-xl">
+                {{-- <figure class="relative"><img src="images/pricing_bg.jpg" alt="" />
+                </figure> --}}
+                <div class="card-body w-full">
+                    <h2 class="card-title text-primary-500 text-4xl">Specialist</h2>
+                    <div class="border my-5 border-primary-600"></div>
+                    <div class="grid grid-cols-2 gap-5  w-full mx-3 text-slate-600 font-semibold">
+                        <p>Symposium</p>
+                        <p>Rp. 1.500.000</p>
+                        <p>Symposium +1
+                            Workshop non
+                            Hands- On</p>
+                        <p>Rp. 2.500.000</p>
+    
+                        <p>Symposium
+                            +1
+                            Workshop
+                            Hands-On</p>
+                        <p>Rp. 3.500.000</p>
+    
+                        <p>Extra
+                          Workshop
+                          Non Hands-
+                          On</p>
+                        <p>Rp. 1.000.000</p>
+                        <p>Extra
+                            Worksho
+                            pHands-
+                            On</p>
+                        <p>Rp. 2.000.000</p>
+                       
+    
+                    </div>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
+    <div class="bg-local pt-24 pb-52 lg:px-20 px-5 bg-blog" id="reg-info">
+        <div class="flex-col flex gap-3 mb-10">
+            <h4 class="text-2xl text-primary-600 font-semibold">Registration</h4>
+            <h1 class="text-4xl text-primary-700 font-semibold">Registration Information</h1>
+        </div>
+        <div class="bg-white border border-gray-200 divide-y divide-gray-200 rounded-lg">
+            <details class="p-6 group" open>
+                <summary class="flex items-center justify-between cursor-pointer">
+                    <h5 class="text-xl font-semibold text-primary-900">
+                        Entitlements 
+                    </h5>
+                    <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+                        <i class="fa-solid fa-eye absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"></i>
+                        <i class="fa-solid fa-eye-slash absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"></i>
+                    </span>
+                </summary>
+        
+                <p class="mt-4 leading-relaxed text-slate-500">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+                    earum similique!
+                </p>
+            </details>
+        
+            <details class="p-6 group">
+                <summary class="flex items-center justify-between cursor-pointer">
+                    <h5 class="text-xl font-semibold text-primary-900">
+                        Registration Fee 
+                    </h5>
+        
+                    <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+                        <i class="fa-solid fa-eye absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"></i>
+                        <i class="fa-solid fa-eye-slash absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"></i>
+                    </span>
+                </summary>
+        
+                <p class="mt-4 leading-relaxed text-slate-500">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+                    earum similique!
+                </p>
+            </details>
+        
+            <details class="p-6 group">
+                <summary class="flex items-center justify-between cursor-pointer">
+                    <h5 class="text-xl font-semibold text-primary-900">
+                        Cancellation Policy of Registration 
+                    </h5>
+        
+                    <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+                        <i class="fa-solid fa-eye absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"></i>
+                        <i class="fa-solid fa-eye-slash absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"></i>
+                    </span>
+                </summary>
+        
+                <p class="mt-4 leading-relaxed text-slate-500">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+                    earum similique!
+                </p>
+            </details>
+        </div>
+        
+    </div>
+
+    
 </div>
--- resources/views/livewire/pages/submission.blade.php
+++ resources/views/livewire/pages/submission.blade.php
@@ -1,11 +1,96 @@
 <div>
+    <div class="bg-local pt-16 pb-52 lg:px-20 px-5 bg-blog">
+        <div class="flex-col flex gap-3 mb-10">
+            <h4 class="text-2xl text-primary-600 font-semibold">Submission</h4>
+            <h1 class="text-4xl text-primary-700 font-semibold">Guideline for Abstracts</h1>
+        </div>
+        <div class="bg-white border border-gray-200 divide-y divide-gray-200 rounded-lg">
+            <details class="p-6 group" open>
+                <summary class="flex items-center justify-between cursor-pointer">
+                    <h5 class="text-xl font-semibold text-primary-900">
+                        Before submitting the abstract you are requested to carefully read the rules regarding abstract submission. 
+                    </h5>
+                    <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+                        <i class="fa-solid fa-eye absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"></i>
+                        <i class="fa-solid fa-eye-slash absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"></i>
+                    </span>
+                </summary>
+        
+                <p class="mt-4 leading-relaxed text-slate-500">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+                    earum similique!
+                </p>
+            </details>
+        
+            <details class="p-6 group">
+                <summary class="flex items-center justify-between cursor-pointer">
+                    <h5 class="text-xl font-semibold text-primary-900">
+                        Abstract submission rules and regulations 
+                    </h5>
+        
+                    <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+                        <i class="fa-solid fa-eye absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"></i>
+                        <i class="fa-solid fa-eye-slash absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"></i>
+                    </span>
+                </summary>
+        
+                <p class="mt-4 leading-relaxed text-slate-500">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+                    earum similique!
+                </p>
+            </details>
+        
+            <details class="p-6 group">
+                <summary class="flex items-center justify-between cursor-pointer">
+                    <h5 class="text-xl font-semibold text-primary-900">
+                        Lorem ipsum dolor sit amet consectetur adipisicing?
+                    </h5>
+        
+                    <span class="relative flex-shrink-0 ml-1.5 w-5 h-5">
+                        <i class="fa-solid fa-eye absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100"></i>
+                        <i class="fa-solid fa-eye-slash absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0"></i>
+                    </span>
+                </summary>
+        
+                <p class="mt-4 leading-relaxed text-slate-500">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+                    earum similique!
+                </p>
+            </details>
+        </div>
+        
+    </div>
     <div class="hero min-h-screen bg-base-200">
         <div class="hero-content flex-col lg:flex-row">
-            <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+            <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+                class="max-w-sm rounded-lg shadow-2xl" />
             <div>
                 <h1 class="text-5xl font-bold">Submission!</h1>
-                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                    exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 <a href="#welcome-message" class="btn btn-primary">Get Started</a>
+            </div>
+            <div class="rating">
+                <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" checked />
+                <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+            </div>
+            <div class="collapse bg-base-200">
+                <input type="radio" name="my-accordion-1" />
+                <div class="collapse-title text-xl font-medium">
+                    Click to open this one and close others
+                </div>
+                <div class="collapse-content">
+                    <p>hello</p>
+                </div>
             </div>
         </div>
     </div>
